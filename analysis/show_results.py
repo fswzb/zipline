9b4751967d331d76336c6e474ea3cba37a5012ef
yunneng <liu.jy.nku@gmail.com>
2016-12-15 21:08:27 +0800

    update analysis/show_results.py

--- analysis/show_results.py
+++ analysis/show_results.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick 
 #%matplotlib inline
 
+
 def drawdown(xs):
     dd = (np.maximum.accumulate(xs) - xs)/np.maximum.accumulate(xs) # drawdown for each point in time
     return dd
@@ -22,7 +23,7 @@ def count(results, benchmark_returns):
     results_count['drawdowns'] = drawdown(results_count['cumulative_returns']+1)
     return results_count
 
-def plot_results(results_count):
+def plot_results(results_count, algo_metrics):
     dates = results_count.index
     algorithm_returns = results_count['algorithm_returns']
     cumulative_returns = results_count['cumulative_returns']
@@ -55,14 +56,40 @@ def plot_results(results_count):
     plt.xlabel('date')
     plt.ylabel('drawdown')
         
+    #algo_metrics = algo.perf_tracker.cumulative_risk_metrics.metrics.iloc[-1]
     algo_return = cumulative_returns[-1]
     year_return = (1+algo_return)**(250./len(dates))-1
     bench_return = benchmark_cumulative_returns[-1]
     bench_year_return = (1+bench_return)**(250./len(dates))-1
     max_drawdown = np.max(drawdowns)
+    alpha = algo_metrics['alpha']
+    beta = algo_metrics['beta']
+    sharpe = algo_metrics['sharpe']
+    algorithm_volatility = algo_metrics['algorithm_volatility']
+    benchmark_volatility = algo_metrics['benchmark_volatility']
+    downside_risk = algo_metrics['downside_risk']
+    sortino = algo_metrics['sortino']
+    information = algo_metrics['information']
+    
+    results_metrics = pd.DataFrame()
+    results_metrics[u'策略收益'] = pd.DataFrame(["%.2f%%" %(algo_return*100)])
+    results_metrics[u'策略年化收益'] = "%.2f%%" %(year_return*100)
+    results_metrics[u'基准收益'] = "%.2f%%" %(bench_return*100)
+    results_metrics[u'基准年化收益'] = "%.2f%%" %(bench_year_return*100)
+    results_metrics[u'最大回撤'] = "%.2f%%" %(max_drawdown*100)
+    results_metrics['alpha'] = np.round(algo_metrics['alpha'],2)
+    results_metrics['beta'] = np.round(algo_metrics['beta'],2)
+    results_metrics['sharpe'] = np.round(algo_metrics['sharpe'],2)
+    results_metrics['algorithm volatility'] = np.round(algo_metrics['algorithm_volatility'],2)
+    results_metrics['benchmark volatility'] = np.round(algo_metrics['benchmark_volatility'],2)
+    results_metrics['downside risk'] = np.round(algo_metrics['downside_risk'],2)
+    results_metrics['sortino'] = np.round(algo_metrics['sortino'],2)
+    results_metrics['information'] = np.round(algo_metrics['information'],2)
     
     #return fig
-    
-    print u'策略收益    策略年化收益     基准收益    基准年化收益    最大回撤'
-    print '%.2f%%        %.2f%%          %.2f%%        %.2f%%        %.2f%% '\
-        %(algo_return*100, year_return*100, bench_return*100, bench_year_return*100, max_drawdown*100)
+    return results_metrics
+    #print u'策略收益    策略年化收益     基准收益    基准年化收益    最大回撤    \
+    #      alpha    beta    sharpe    algorithm_volatility    '
+    #print '%.2f%%        %.2f%%          %.2f%%        %.2f%%        %.2f%% '\
+    #    %(algo_return*100, year_return*100, bench_return*100, bench_year_return*100, max_drawdown*100)
+        
